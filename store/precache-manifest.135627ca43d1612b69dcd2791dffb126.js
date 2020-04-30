3c10c448d164f31237a1868f679ffa8e01f9ee65
magabiev <magabiev1401@mail.ru>
2020-04-30 12:54:49 +0300

    first commit

---
+++ store/precache-manifest.135627ca43d1612b69dcd2791dffb126.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b69d6b5c2fe99fc0723e",
+    "url": "./static/js/main.1f76360a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1eca06a644c849530f45",
+    "url": "./static/js/2.52a5005f.chunk.js"
+  },
+  {
+    "revision": "fe5b69bac2e453f61d93fe9efea17ac3",
+    "url": "./index.html"
+  }
+];
