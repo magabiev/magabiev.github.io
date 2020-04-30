dd9211089f9db295061297a0d22839a5a68ff071
magabiev <magabiev1401@mail.ru>
2020-04-30 22:04:55 +0300

    chat in react

---
+++ chat/precache-manifest.c0cf82cbaefc7efabced1b2ad5f7eae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c19416deeb44306523b8",
+    "url": "./static/css/main.23d5b2e4.chunk.css"
+  },
+  {
+    "revision": "c19416deeb44306523b8",
+    "url": "./static/js/main.5dcb3936.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bea998b0054f7d472be2",
+    "url": "./static/css/2.2d19626e.chunk.css"
+  },
+  {
+    "revision": "bea998b0054f7d472be2",
+    "url": "./static/js/2.bcc27e93.chunk.js"
+  },
+  {
+    "revision": "c9fe3de1c0ead629cd12a455d0564f0f",
+    "url": "./index.html"
+  }
+];
